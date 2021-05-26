e18a328f63985d559ce9ee57d245a5d46264606d
Anthony Lannutti <lannuttia@gmail.com>
2021-05-26 16:21:53 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
